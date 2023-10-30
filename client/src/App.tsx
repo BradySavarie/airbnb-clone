5492bc92fe1dadec958a890bfe2f5a31a2462d19
Brady <brady.savarie@hotmail.com>
2023-10-29 23:30:20 -0400

    Setup layout component and login page

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,11 +1,16 @@
 import { Routes, Route } from 'react-router';
 import './App.css';
-import IndexPage from './pages/IndexPage';
+import IndexPage from '../src/pages/IndexPage';
+import LoginPage from '../src/pages/LoginPage';
+import Layout from './Layout';
 
 function App() {
     return (
         <Routes>
-            <Route index element={<IndexPage />} />
+            <Route path="/" element={<Layout />}>
+                <Route index element={<IndexPage />} />
+                <Route path="/login" element={<LoginPage />} />
+            </Route>
         </Routes>
     );
 }
