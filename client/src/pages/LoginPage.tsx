d465cd40fd351ca3a0d360c52fe1af4202e7de03
Brady <brady.savarie@hotmail.com>
2023-10-30 23:26:29 -0400

    Small styling adjustments

--- client/src/pages/LoginPage.tsx
+++ client/src/pages/LoginPage.tsx
@@ -3,12 +3,12 @@ import { Link } from 'react-router-dom';
 export default function LoginPage() {
     return (
         <div className="mt-4 flex grow items-center justify-around">
-            <div className="mb-64">
+            <div className="mb-36">
                 <h1 className="text-4xl text-center mb-4">Login</h1>
                 <form action="" className="max-w-md mx-auto">
                     <input type="email" placeholder="your@email.com" />
                     <input type="password" placeholder="password" />
-                    <button className="primary">Login</button>
+                    <button className="primary mt-4">Login</button>
                     <div className="text-center py-2 text-gray-500">
                         Don't have an account?{' '}
                         <Link className="underline text-black" to={'/register'}>
--- client/src/pages/RegisterPage.tsx
+++ client/src/pages/RegisterPage.tsx
@@ -3,13 +3,13 @@ import { Link } from 'react-router-dom';
 export default function RegisterPage() {
     return (
         <div className="mt-4 flex grow items-center justify-around">
-            <div className="mb-64">
+            <div className="mb-36">
                 <h1 className="text-4xl text-center mb-4">Register</h1>
                 <form action="" className="max-w-md mx-auto">
                     <input type="text" placeholder="John Doe" />
                     <input type="email" placeholder="your@email.com" />
                     <input type="password" placeholder="password" />
-                    <button className="primary">Login</button>
+                    <button className="primary mt-4">Login</button>
                     <div className="text-center py-2 text-gray-500">
                         Already a member?{' '}
                         <Link className="underline text-black" to={'/login'}>
