3390cf3237a77426203e2d5971c038021818e637
Brady <brady.savarie@hotmail.com>
2023-11-03 23:37:04 -0400

    Create account subpage navigation

--- client/src/pages/AccountPage.tsx
+++ client/src/pages/AccountPage.tsx
@@ -1,12 +1,17 @@
 import { useContext } from 'react';
 import { UserContext } from '../context/UserContext';
-import { Navigate } from 'react-router';
+import { Navigate, useParams } from 'react-router';
+import { Link } from 'react-router-dom';
 
 export default function AccountPage() {
     const contextValue = useContext(UserContext);
-    console.log(contextValue);
-    if (!contextValue) return null;
 
+    let { subpage } = useParams();
+    if (subpage === undefined) {
+        subpage = 'profile';
+    }
+
+    if (!contextValue) return null;
     const { user, ready } = contextValue;
 
     if (!ready) {
@@ -17,5 +22,33 @@ export default function AccountPage() {
         return <Navigate to={'/login'} />;
     }
 
-    return <div>Account Page for {user!.name}</div>;
+    function linkClasses(type: string | null) {
+        let classes = 'py-2 px-6';
+        if (type === subpage) {
+            classes += ' bg-primary text-white rounded-full';
+        }
+        return classes;
+    }
+
+    return (
+        <div>
+            <nav className="w-full flex mt-8 gap-2 justify-center">
+                <Link className={linkClasses('profile')} to={'/account'}>
+                    My profile
+                </Link>
+                <Link
+                    className={linkClasses('bookings')}
+                    to={'/account/bookings'}
+                >
+                    My bookings
+                </Link>
+                <Link
+                    className={linkClasses('accommodations')}
+                    to={'/account/accommodations'}
+                >
+                    My accommodations
+                </Link>
+            </nav>
+        </div>
+    );
 }
